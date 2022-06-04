1a45b003211b5aedb310c869495ef9c697dacc20
satwik1608 <satwikkashyap1608@gmail.com>
2022-06-04 20:26:18 +0530

    ad

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get("/me", auth, async (req, res) => {
   res.send(user);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", [auth, admin], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
